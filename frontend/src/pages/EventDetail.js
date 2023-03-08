b0a771e88c678126f8b3ac390772458962a4980b
LordCracken <vladislav070201@gmail.com>
2023-03-09 00:48:40 +0300

    use route params in event detail page

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,3 +1,14 @@
-const EventDetailPage = () => <h1>Event Detail Page</h1>;
+import { useParams } from 'react-router-dom';
+
+const EventDetailPage = () => {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Event Detail Page</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
+};
 
 export default EventDetailPage;
