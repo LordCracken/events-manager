3d44f861e00c1ea1cd119ce3f5e205fb8e85fe3d
LordCracken <vladislav070201@gmail.com>
2023-03-27 17:10:13 +0300

    add feedback for events list loading

--- frontend/src/pages/events/Events.js
+++ frontend/src/pages/events/Events.js
@@ -1,21 +1,30 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 import EventsList from '../../components/EventsList';
 
 const EventsPage = () => {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>{loadedEvents => <EventsList events={loadedEvents} />}</Await>
+    </Suspense>
+  );
 };
 
-export const loader = async () => {
+const loadEvents = async () => {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     return json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+};
+
+export const loader = () => {
+  return defer({ events: loadEvents() });
 };
 
 export default EventsPage;
